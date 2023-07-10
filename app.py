468600fdc1077d3f6835071ee5bfe459e74244b5
Sebastian Hresko <sebastian.hresko@telekom.com>
2023-07-10 15:10:44 +0200

    hresko commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from webex_bot.webex_bot import WebexBot
 from gpt import gpt
 
-bot = WebexBot("Your webex bot token")
+bot = WebexBot("ZGU5MjEwZjctNGEwYi00MzlmLWJlZDAtODJiOTAyZTdlNGI0YTFlMmZjZWItNjNi_PE93_b6c69aac-d199-4628-980a-d750015d67a5")
 bot.commands.clear()
 bot.add_command(gpt())
 bot.help_command = gpt()
--- gpt.py
+++ gpt.py
@@ -5,22 +5,21 @@ import openai
 class gpt(Command):
 
     messages = []
-    messages.append({"role":"system","content":"You are polite assistant named YOURNAME."})
+    messages.append({"role":"system","content":"You are polite assistant named Peter."})
 
     def __init__(self):
         super().__init__()
-    
+
     def execute(self, message, attachment_actions, activity):
-        
+
         openai.api_type = "azure"
-        openai.api_version = "Your Azure OpenAI resource's version value." # for example: 2020-07-01-preview
-        openai.api_base = "Your Azure OpenAI resource's endpoint value ." 
-        openai.api_key = "Your Azure OpenAI resource's key value."
-        
+        openai.api_version = "2023-03-15-preview" # for example: 2020-07-01-preview
+        openai.api_base = "https://cs-gpt3-research.openai.azure.com/"
+        openai.api_key = "fae147496ee146708bac9f46159154a3"
+
         self.messages.append({"role": "user", "content": message})
         completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        engine="model-chat",
+        engine="cs-gpt35-turbo",
         messages=self.messages)
 
         gpt_response=completion.choices[0].message.content
